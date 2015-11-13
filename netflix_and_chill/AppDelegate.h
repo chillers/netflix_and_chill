0627103fcf83be5e0e319bb1b3ae415b59f0eb31
Prasanth Guruprasad <nagu89@gmail.com>
2015-11-12 16:04:10 -0800

    basic skeleton

---
+++ netflix_and_chill/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  netflix_and_chill
+//
+//  Created by Prasanth Guruprasad on 11/12/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ netflix_and_chill/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  netflix_and_chill
+//
+//  Created by Prasanth Guruprasad on 11/12/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
